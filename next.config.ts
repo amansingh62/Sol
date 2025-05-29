764a8f7ba5bc116ab3efbcfc4fa7d0d8d0b2e92f
Aman Singh <coolaman6299@gmail.com>
2025-05-29 13:26:31 +0530

    Added export to config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // other config options here
 };
 
 export default nextConfig;
+
